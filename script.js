a93f8225c37496970338ff46afb012b592355d46
jeel-patel-04 <161931618+jeel-patel-04@users.noreply.github.com>
2025-09-20 12:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -150,3 +150,27 @@ window.history.replaceState(null, null, window.location.pathname);
 window.addEventListener("load", () => {
   window.scrollTo(0, 0);
 });
+// Particles background
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#a100ff" },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.5 },
+    "size": { "value": 3, "random": true },
+    "line_linked": { "enable": true, "distance": 150, "color": "#8000ca", "opacity": 0.4, "width": 1 },
+    "move": { "enable": true, "speed": 2 }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" },
+      "onclick": { "enable": true, "mode": "push" }
+    },
+    "modes": {
+      "repulse": { "distance": 100 },
+      "push": { "particles_nb": 4 }
+    }
+  },
+  "retina_detect": true
+});
